1628669ed6503b4e42ad90c1d19042f332987ce7
Kappie <ghkapteijns@gmail.com>
2014-05-02 14:41:32 +0200

    dat moest natuurlijk anders: in de controller wordt nu besloten welke template er wordt gerenderd.

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,11 @@
 class WelcomeController < ApplicationController
   def index
+    if student_signed_in?
+      render "student_index"
+    elsif teacher_signed_in?
+      render "teacher_index"
+    else
+      render "index"
+    end
   end
 end
